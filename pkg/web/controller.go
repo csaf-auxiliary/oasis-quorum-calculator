2bb5f6a435b5e731dc85d139ff3cb7d7379fa908
JanHoefelmeyer <jan.hoefelmeyer@intevation.de>
2025-05-15 19:28:35 +0200

    Add export csv button

--- pkg/web/controller.go
+++ pkg/web/controller.go
@@ -153,6 +153,7 @@ func (c *Controller) Bind() http.Handler {
 		{"/meeting_status", mw.CommitteeRoles(c.meetingStatus, models.ChairRole, models.MemberRole, models.SecretaryRole)},
 		{"/meeting_status_store", mw.CommitteeRoles(c.meetingStatusStore, models.ChairRole, models.SecretaryRole)},
 		{"/meeting_attend_store", mw.CommitteeRoles(c.meetingAttendStore, models.ChairRole, models.SecretaryRole)},
+		{"/meetings_export", mw.CommitteeRoles(c.meetingsExport, models.ChairRole, models.SecretaryRole)},
 		// Member
 		{"/member", mw.Roles(c.member, models.MemberRole)},
 		{"/member_attend", mw.CommitteeRoles(c.memberAttend, models.MemberRole)},
